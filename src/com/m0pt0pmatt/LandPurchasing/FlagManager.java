b418b45e53c03e297cb4341ccce406c199094178
m0pt0pmatt <m0pt0pmatt17@gmail.com>
2014-01-10 23:15:06 -0700

    save worldguard after altering flags

--- src/com/m0pt0pmatt/LandPurchasing/FlagManager.java
+++ src/com/m0pt0pmatt/LandPurchasing/FlagManager.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 import com.m0pt0pmatt.LandPurchasing.flags.LandFlag;
+import com.sk89q.worldguard.protection.databases.ProtectionDatabaseException;
 import com.sk89q.worldguard.protection.flags.BooleanFlag;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import com.sk89q.worldguard.protection.flags.Flag;
@@ -211,7 +212,12 @@ public class FlagManager {
 		}
 		
 		sender.sendMessage("You have successfully set flag " + flagName + " to " + value + " for the plot " + plotName + " for a cost of $" + cost);
-		
+		try {
+			rm.save();
+		} catch (ProtectionDatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return true;	
 	}
 	
