3ab309f739674af8dd90d68217e6ee0e607bed79
Skyler <skymanzanares@hotmail.com>
2015-05-29 15:48:01 -0600

    Fleshed out toConfig and fromConfig methods for LeaseLand

--- src/com/m0pt0pmatt/LandPurchasing/LeaseLand.java
+++ src/com/m0pt0pmatt/LandPurchasing/LeaseLand.java
@@ -1,10 +1,13 @@
 package com.m0pt0pmatt.LandPurchasing;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.m0pt0pmatt.LandPurchasing.Scheduling.Date;
+import com.sk89q.worldedit.BlockVector;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 /**
  * Holds information about a plot of land that is able to be or is being leased out.<br />
@@ -27,31 +30,48 @@ public class LeaseLand extends Land {
 	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X: 312<br />
 	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Y: 70<br />
 	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Z: -700<br />
-	 * &nbsp;&nbsp;&nbsp;&nbsp;Date:
-	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SerializedDateStuff
+	 * &nbsp;&nbsp;&nbsp;&nbsp;Date:<br />
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Day: 14<br />
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Month: 09<br />
+	 * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Year: 2014<br />
 	 * </p>
 	 * @param section The section to load from
 	 * @return The resultant LeaseLand object, or <i>null</i> if there was an error
 	 */
-	public static LeaseLand fromConfig(ConfigurationSection section) {
+	public static LeaseLand fromConfig(String name, ConfigurationSection section) {
 		if (section == null) {
 			return null;
 		}
 		
 		//TODO check and make sure that region doesn't conflict with any others!
-		LeaseLand land = new LeaseLand();
+		
+		//load up info from config
+		BlockVector b1, b2;
+		b1 = new BlockVector(section.getInt("Block1.X"), section.getInt("Block1.Y"), section.getInt("Block1.Z"));
+		b2 = new BlockVector(section.getInt("Block2.X"), section.getInt("Block2.Y"), section.getInt("Block2.Z"));
 		
 		
+		ProtectedCuboidRegion region = new ProtectedCuboidRegion(name, b1, b2);
 		
-		return land;
+		
+		LandPurchasing.wgplugin.getRegionManager(Bukkit.getWorld("Homeworld")).addRegion(region);
+		
+
+		LeaseLand lease = new LeaseLand(region);
+		
+		if (section.contains("Date") && section.contains("Date.Day") && section.contains("Date.Month") && section.contains("Date.Year")) {
+			//due date information!
+			lease.dueDate = new Date(
+					section.getInt("Date.Day"),
+					section.getInt("Date.Month"),
+					section.getInt("Date.Year")
+					);
+		}
+		//else do nothing, as assumed no date info on creation
+		
+		return lease;
 	}
 	
-	/**
-	 * Constructor-method use only!
-	 */
-	private LeaseLand() {
-		super();
-	}
 	
 	public LeaseLand(ProtectedCuboidRegion region) {
 		super();
